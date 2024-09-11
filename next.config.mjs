21a706467ce458da2b5df830352bfb16ec5be6b7
RicardoAgk <macos@Macs-MacBook-Air.local>
2024-09-11 09:38:48 +0100

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
+import SideNav from '@/app/ui/dashboard/sidenav';
 
 const nextConfig = {};
 
 export default nextConfig;
+export const experimental_ppr = true;
